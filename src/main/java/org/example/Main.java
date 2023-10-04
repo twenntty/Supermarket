65c71324240a0461d1df920f20177574778469a8
Mark Bilous <belousme601@gmail.com>
2023-10-04 15:47:06 +0300

    Add class cart

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,4 +15,36 @@ class Product {
         this.price = price;
     }
 
+    public int getId() {
+        return  id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
+
+class Cart {
+    private  List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public  List<Product> getProducts() {
+        return products;
+    }
 }
