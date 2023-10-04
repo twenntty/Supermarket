d67d5bbe8b36a62dd6edf6e8ca5d7cad44d588b9
Mark Bilous <belousme601@gmail.com>
2023-10-04 15:38:37 +0300

    Add class product

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import  java.util.ArrayList;
+import java.util.List;
+
+class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+}
